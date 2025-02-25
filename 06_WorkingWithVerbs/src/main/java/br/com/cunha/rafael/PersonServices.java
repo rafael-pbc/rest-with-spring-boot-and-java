38c99aa3234006ab891bfb96d0939a673cfc183c
Rafael Cunha <rafael.bispocunha@outlook.com>
2025-02-25 12:55:11 -0300

    Working with four HTTP Main Verbs - GET, POST, PUT and DELETE

---
+++ 06_WorkingWithVerbs/src/main/java/br/com/cunha/rafael/PersonServices.java
@@ -0,0 +1,69 @@
+package br.com.cunha.rafael;
+
+import br.com.cunha.rafael.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+
+        for(int i = 0; i < 8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    public Person findById(String id){
+        logger.info("Finding one Person!");
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Rafael");
+        person.setLastName("Cunha");
+        person.setAddress("São Paulo, SP - Brazil");
+        person.setGender("Male");
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Firstname " + i);
+        person.setLastName("Lastname " + i);
+        person.setAddress("Some Address in Brazil");
+        person.setGender("Male");
+        return person;
+    }
+
+    public Person create(Person person){
+
+        logger.info("Creating one Person!");
+
+        return person;
+    }
+
+    public Person update(Person person){
+
+        logger.info("Updating one Person!");
+
+        return person;
+    }
+
+    public void delete(String id){
+
+        logger.info("Deleting one Person!");
+        
+    }
+
+}
