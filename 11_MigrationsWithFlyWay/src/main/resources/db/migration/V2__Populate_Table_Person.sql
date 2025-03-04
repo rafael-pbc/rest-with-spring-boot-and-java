559465411b75f42ca0e2b2f4310ea59dcccef299
Rafael Cunha <rafael.bispocunha@outlook.com>
2025-03-04 11:58:33 -0300

    Adding migrations support with Flyway

---
+++ 11_MigrationsWithFlyWay/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Rafael', 'Male', 'Cunha'),
+	(2, 'Anchiano - Italy -1500', 'Leonardo', 'Male', 'da Vinci'),
+	(5, 'Kentucky - US', 'Muhamamd', 'Male', 'Ali'),
+	(7, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(8, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(9, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(10, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla'),
+	(11, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
