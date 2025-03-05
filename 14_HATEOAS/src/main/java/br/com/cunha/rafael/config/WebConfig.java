38422c916ba1bbfe8ef7e365eda5644b120067c7
Rafael Cunha <rafael.bispocunha@outlook.com>
2025-03-05 10:29:52 -0300

    Testing Generation of HATEOAS Links with Mockito

---
+++ 14_HATEOAS/src/main/java/br/com/cunha/rafael/config/WebConfig.java
@@ -0,0 +1,35 @@
+package br.com.cunha.rafael.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        // via EXTENSION. http://localhost:8080/api/person/v1/1.xml or
+        // http://localhost:8080/api/person/v1/1.JSON Deprecated on Spring Boot
+
+        //Via QUERY PARAM http://localhost:8080/api/person/v1/1?mediaType=xml
+        /*
+        configurer.favorParameter(true)
+                .parameterName("mediaType")
+                .ignoreAcceptHeader(true).useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+         */
+
+        //Via HEADER PARAM http://localhost:8080/api/person/v1/1
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false).useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("yaml", MediaType.APPLICATION_YAML);
+    }
+}
